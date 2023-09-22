e4995a8667aa1bdb5c1a692caf4e667943cc1592
KellyCuna0065 <TI20110065@itess.edu.mx>
2023-09-22 16:43:36 -0500

    add hellogithub

--- src/main/main.java
+++ src/main/main.java
@@ -6,10 +6,14 @@ package main;
 
 /**
  *  ITESS-TICS
- * Taller de inveci
+ * Taller de investigacion
  * Intento de curso de certificacion java 0Z1-808
+ * 22 / septiembre / 2023
  * @author Kelly Cuna
  */
 public class main {
-    
+    public static void main(String[] args) {
+        // to do
+        System.out.println("hi c:");
+    }
 }
