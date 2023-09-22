283d0803d4406fc40fef2697f2f545fafb2cd7e8
KellyCuna0065 <TI20110065@itess.edu.mx>
2023-09-22 16:28:55 -0500

    first commit

---
+++ src/main/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *  ITESS-TICS
+ * Taller de inveci
+ * Intento de curso de certificacion java 0Z1-808
+ * @author Kelly Cuna
+ */
+public class main {
+    
+}
